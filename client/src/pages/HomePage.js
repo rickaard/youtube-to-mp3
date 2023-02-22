af6c66fb13f56224a9933457db72354b0803a873
Rickard <rickaard@gmail.com>
2023-02-22 23:54:44 +0100

    Let API serve React app in prod

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,102 +1,109 @@
-import React, { useState } from 'react';
-import FormWrapper from '../components/FormWrapper/FormWrapper';
-import ConvertWrapper from '../components/ConvertWrapper/ConvertWrapper';
-import Loader from '../components/Loader/Loader';
-
-import download from 'downloadjs';
-
+import React, { useState } from "react";
+import FormWrapper from "../components/FormWrapper/FormWrapper";
+import ConvertWrapper from "../components/ConvertWrapper/ConvertWrapper";
+import Loader from "../components/Loader/Loader";
+import download from "downloadjs";
 
 const HomePage = () => {
-    const [showVideo, setShowVideo] = useState(false);
-    const [videolink, setVideolink] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [youtubeID, setYoutubeID] = useState(null);
-    const [youtubeTitle, setYoutubeTitle] = useState('');
-    const [error, setError] = useState(null);
+  const [showVideo, setShowVideo] = useState(false);
+  const [videolink, setVideolink] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [youtubeID, setYoutubeID] = useState(null);
+  const [youtubeTitle, setYoutubeTitle] = useState("");
+  const [error, setError] = useState(null);
 
-    const getVideo = (link, id) => {
-        setIsLoading(true);
-        setYoutubeID(id)
+  const getVideo = (link, id) => {
+    setIsLoading(true);
+    setYoutubeID(id);
 
-        // POST youtube ID to server
-        // get back link with youtube movie downloaded to mp4
-        fetch(process.env.REACT_APP_API_VIDEO, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ ytid: id })
-        })
-            .then(response => response.json())
-            .then(data => {
-                setVideolink(data.link);
-                setIsLoading(false);
-                setShowVideo(true);
-                setYoutubeTitle(data.title);
-            })
-            .catch(err => console.log(err))
+    // POST youtube ID to server
+    // get back link with youtube movie downloaded to mp4
+    fetch("/api/video/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ytid: id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setVideolink(data.link);
+        setIsLoading(false);
+        setShowVideo(true);
+        setYoutubeTitle(data.title);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    }
+  const goBackToStart = () => {
+    setShowVideo(false);
+    setVideolink(null);
+    setYoutubeID(null);
+    setYoutubeID(null);
+    setYoutubeTitle("");
+    setError(null);
+  };
 
-    const goBackToStart = () => {
-        setShowVideo(false);
-        setVideolink(null);
-        setYoutubeID(null);
-        setYoutubeID(null);
-        setYoutubeTitle('');
-        setError(null);
-    }
+  const convertToMp3 = (starttime, endtime) => {
+    setIsLoading(true);
 
-    const convertToMp3 = (starttime, endtime) => {
-        setIsLoading(true);
+    fetch("/api/convert/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ytid: youtubeID,
+        starttime,
+        endtime,
+        title: youtubeTitle,
+      }),
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("File not found");
+        }
+        return response.blob();
+      })
+      .then((data) => {
+        download(
+          data,
+          `${youtubeTitle}.mp3`,
+          "Content-Disposition: attachment"
+        );
+        goBackToStart();
+      })
+      .catch((err) => {
+        setError(
+          "File does not exists anymore, try again by refreshing the page"
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-        fetch(process.env.REACT_APP_API_CONVERT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ytid: youtubeID,
-                starttime,
-                endtime,
-                title: youtubeTitle
-            })
-        })
-            .then(response => {
-                if (response.status !== 200) {
-                    throw new Error('File not found');
-                }
-                return response.blob();
-            })
-            .then(data => {
-                download(data, `${youtubeTitle}.mp3`, 'Content-Disposition: attachment');
-                goBackToStart();
-            })
-            .catch(err => {
-                setError('File does not exists anymore, try again by refreshing the page');
-            })
-            .finally(() => {
-                setIsLoading(false);
-            })
-    }
+  if (isLoading) {
+    return <Loader />;
+  }
 
+  if (error) {
+    return <div style={{ textAlign: "center" }}>{error}</div>;
+  }
 
-    if (isLoading) {
-        return <Loader />
-    }
+  return (
+    <>
+      {!showVideo ? (
+        <FormWrapper getVideo={getVideo} />
+      ) : (
+        <ConvertWrapper
+          videolink={videolink}
+          goBackToStart={goBackToStart}
+          convertToMp3={convertToMp3}
+        />
+      )}
+    </>
+  );
+};
 
-    if (error) {
-        return <div style={{ textAlign: 'center' }}>{error}</div>
-    }
-
-    return (
-        <>
-            {!showVideo
-                ? <FormWrapper getVideo={getVideo} />
-                : <ConvertWrapper videolink={videolink} goBackToStart={goBackToStart} convertToMp3={convertToMp3} />
-            }
-        </>
-    )
-}
-
-export default HomePage
+export default HomePage;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
-import { checkFileExists, removeFile, convertToMp3, downloadYoutubeVideo } from "./converter";
+import {
+  checkFileExists,
+  removeFile,
+  convertToMp3,
+  downloadYoutubeVideo,
+} from "./converter";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/temp", express.static("temp"));
+
+if (process.env.NODE_ENV === "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = Path.dirname(__filename);
+
+  app.use(express.static(Path.join(__dirname, "client/build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(Path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.get("/api", (req, res) => {
   res.status(200).json({ message: "hello" });
@@ -34,7 +49,13 @@ app.post("/api/video", async (req, res) => {
       }
     }, FIVE_MINUTES);
 
-    return res.status(200).json({ status: "ok", message: "downloaded video", link: video.filepath });
+    return res
+      .status(200)
+      .json({
+        status: "ok",
+        message: "downloaded video",
+        link: video.filepath,
+      });
   } catch (err) {
     console.log(err);
 
@@ -49,7 +70,11 @@ app.post("/api/convert", async (req, res) => {
     const convertedFile = await convertToMp3(ytid, starttime, endtime, title);
 
     res.download(`temp/${convertedFile}`, (err) => {
-      if (err) return console.log("Something went wrong while trying to download", err);
+      if (err)
+        return console.log(
+          "Something went wrong while trying to download",
+          err
+        );
 
       fs.unlink(`${__dirname}/temp/${ytid}.mp3`, () => {
         console.log("mp3-file deleted");
@@ -70,4 +95,6 @@ app.post("/api/convert", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => console.log(`Server is upp and running on port: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is upp and running on port: ${PORT}`)
+);
